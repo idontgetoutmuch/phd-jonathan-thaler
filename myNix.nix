01e01568be00f600899ccc8e2a9bea9a14e401dd
Hrishika <81599734+hrishika-alaj@users.noreply.github.com>
2023-03-02 19:47:42 +1000

    Version of notebook with attempt at reading CSV file
    Code for the plot and reading of csv is at the bottom of the notebook (see either the .md or the .ipynb files). The nix script used is also included (using csv and cassava in this version).

--- myNix.nix
+++ myNix.nix
@@ -1,15 +1,39 @@
-{ nixpkgs ? import <nixpkgs> { } }:
-
 let
+  overlay = self: super: {
+    haskell = super.haskell // {
+      packageOverrides = selfH: superH: {
 
-haskellDeps = ps: with ps; [
-  array base bearriver containers dunai gloss MonadRandom mtl hvega ihaskell-hvega ];
+        gloss-export = super.haskell.lib.doJailbreak (super.haskell.lib.dontCheck (
+          selfH.callCabal2nix "gloss-export" (builtins.fetchGit {
+            url = "https://gitlab.com/timo-a/gloss-export.git";
+            rev = "d2d38aa679f31838ff718ffaac1fd9459b5c925f";
+          }) { }));
 
+        ihaskell-diagrams = selfH.callCabal2nixWithOptions "ihaskell-diagrams" (builtins.fetchGit {
+          url = "https://github.com/IHaskell/IHaskell";
+          rev = "725d900414462da0f1859334a482e80c7a9e33d9";
+          # rev = "1c22a874ac0c8ed019229f4a0cd5a0bfda017357";
+        }) "--subpath ihaskell-display/ihaskell-diagrams" { };
 
-
-in
-
-import ./release.nix {
-  compiler = "ghc902";
-  packages = haskellDeps;
+      };
+    };
+  };
+  nixpkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/22.11.tar.gz";
+    sha256 = "11w3wn2yjhaa5pv20gbfbirvjq6i3m7pqrq2msf0g7cv44vijwgw";
+  }) { overlays = [ overlay ]; };
+in import ./release-9.0.nix {
+  nixpkgs = nixpkgs; # equivalent to `inherit nixpkgs;`
+  packages = haskellPackages: with haskellPackages; [
+    array base bearriver Chart containers diagrams dunai gloss
+    MonadRandom mtl diagrams diagrams-cairo hvega ihaskell-hvega
+    plots
+    ihaskell-juicypixels
+    gloss-export
+    ihaskell-diagrams
+    diagrams-cairo
+    diagrams-rasterific
+    csv
+    cassava
+  ];
 }
